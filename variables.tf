d6a0dd63507f9dfff29494e37960bf2b4ed17333
nathan Chufor <chufor.nathan@yahoo.com>
2022-09-05 20:30:01 -0700

    added some changes to variables

--- variables.tf
+++ variables.tf
@@ -1,13 +1,12 @@
 variable "region" {
     default = "us-east-1"
-  
 }
-
 variable "environment" {
-    default = "Development"
-  
+    default = "Development"  
 }
 variable "vpc_cidr" {
-    description = "vpc cidr"
-  
+    description = "vpc cidr"  
+}
+variable "public_subnet_1_cidr" {
+    description = "public subnet 1 cidr"
 }
