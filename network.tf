8b617ef610ac37f3126b7304aed8a0f28631f2b4
nathan Chufor <chufor.nathan@yahoo.com>
2022-09-06 19:11:11 -0700

    changes

--- network.tf
+++ network.tf
@@ -14,11 +14,3 @@ resource "aws_subnet" "public-subnet-1" {
     Name = "${var.environment}-public-subnet-1"
   }
 }
-resource "aws_subnet" "public-subnet-2" {
-  cidr_block        = "${var.public_subnet_2_cidr}"
-  vpc_id            = "${aws_vpc.development-vpc.id}"
-  availability_zone = "${var.region}b"
-  tags = {
-    Name = "${var.environment}-Public-Subnet-2"
-  }
-}
